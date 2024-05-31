bb442e8a11791b5b2dd79d96e8fe4ddd7ca12401
Tue Phan Vo Minh <mingtue04@gmail.com>
2024-05-31 11:27:57 +0700

    base front-end (navbar, signin signup, footer, hero)

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Hero from "../Components/Hero/Hero.jsx"
+import Footer from "../Components/Footer/Footer.jsx"
+
+const Shop = () => {
+    return (
+        <div>
+            <Hero />
+            <br></br>
+            <br></br>
+            <br></br>
+            <Footer />
+        </div>
+    )
+}
+
+export default Shop
