bf1a26c6b7f14f6f4fe3a506400b53b25e56b3e3
Tue Phan Vo Minh <mingtue04@gmail.com>
2024-06-27 16:40:49 +0700

    Done features: search, filter, sort, related-product

--- server.js
+++ server.js
@@ -2,30 +2,34 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import connectDB from "./config/db.js";
-import authRoute from './routes/authRoute.js';
-import cors from 'cors'
+import connectDB from "./Backend/config/db.js";
+import authRoute from './Backend/routes/authRoute.js';
+import productRoute from './Backend/routes/productRoute.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-//database config
+// Connect to MongoDB
 connectDB();
 
-//middlewares
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-//routes
+// Routes
 app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/products', productRoute);
 
+// Default route
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to ecommerce project</h1>');
+    res.send('<h1>Minoshoes Store</h1>');
 });
 
+// Start server
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`.bgCyan.white);
-})
+});
