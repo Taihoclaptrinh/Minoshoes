00a29b55a744da27875dc6f638a068f7a0ab3b98
NQTin <120164439+NQTin@users.noreply.github.com>
2024-08-10 20:05:56 +0700

    debug(Gallary.jsx)

--- Frontend/src/Components/Gallary/Gallary.jsx
+++ Frontend/src/Components/Gallary/Gallary.jsx
@@ -6,18 +6,8 @@ import Pop_up from "../Popup_PayOS/Popup";
 import ProductSlider from "./ProductSlider";
 
 const Gallary = () => {
-  const [showPopup, setShowPopup] = useState(false);
-  const navigate = useNavigate();
+
   const hotshoe4 = "https://minoshoesstorage.blob.core.windows.net/minoshoesbackground/jordan-ng.jpeg";
-
-  const handleExploreClick = () => {
-    setShowPopup(true);
-  };
-
-  const closePopup = () => {
-    setShowPopup(false);
-    navigate("/"); // Redirect to the landing page
-  };
 
   return (
     <div className="layout">
@@ -31,12 +21,11 @@ const Gallary = () => {
           <Link to="/new-arrivals" className="Gal_left-section">
             Shop
           </Link>
-          <div className="Gal_right-section" onClick={handleExploreClick}>
+          <div className="Gal_right-section">
             Explore
           </div>
         </div>
       </div>
-      {showPopup && <Pop_up review="Thanh toán thành công!" onClose={closePopup} />}
     </div>
   );
 };
